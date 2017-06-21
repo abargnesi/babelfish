4a67aa4502e42f47634fc7a61af0e2a9c9e33891
Anthony Bargnesi <abargnesi@crimsonhexagon.com>
2017-06-21 08:50:41 -0400

    FS helpers

--- _FS/aliases.sh
+++ _FS/aliases.sh
@@ -2,3 +2,6 @@
 
 alias FSt='tail -f'
 alias FSt__help='echo "Continuously tail log."'
+
+alias FSdu='du --max-depth=1 -h'
+alias FSdu__help='echo "Calculate disk usage for files in dir."'
--- _FS/functions.sh
+++ _FS/functions.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-
+function FSff() {
+    if [ $# -eq 1 ]; then
+        find . -type f -name "$1"
+    else
+        find . -type f
+    fi
+}
+alias FSff__help='echo "Find files from this directory."'
+export -f FSff
