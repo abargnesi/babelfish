746517700276b2c3a92dc2dc3290a6f3222a5b3a
Anthony Bargnesi <abargnesi@crimsonhexagon.com>
2017-07-11 14:33:05 -0400

    network vocabulary (N)

---
+++ _N/aliases.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+alias Npc='nc -zv 127.0.0.1'
+alias Npc__help='echo "Check for open ports on local machine."'
+
+# vim: ts=4 sts=4 sw=4 expandtab
---
+++ _N/functions.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+# vim: ts=4 sts=4 sw=4 expandtab
